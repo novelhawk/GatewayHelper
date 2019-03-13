0f893f1c49bbaa0a6478652903f4071c4a68226f
Hawk of the Death <italia195@outlook.it>
2019-03-13 19:24:29 +0100

    Explicitly declaring StdCall calling convention on ADL interfaces.

--- GatewayChanger/Native/IIPHelper.cs
+++ GatewayChanger/Native/IIPHelper.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using AdvancedDLSupport;
 using GatewayChanger.Native.Structures;
 
 namespace GatewayChanger.Native
 {
     public interface IIPHelper
     {
+        [NativeSymbol(CallingConvention = CallingConvention.StdCall)]
         int GetIpForwardTable(IntPtr pIpForwardTable, ref IntPtr pdwSize, bool bOrder);
+        
+        [NativeSymbol(CallingConvention = CallingConvention.StdCall)]
         int CreateIpForwardEntry(ref IpForwardRow pRoute);
+        
+        [NativeSymbol(CallingConvention = CallingConvention.StdCall)]
         int DeleteIpForwardEntry(ref IpForwardRow pRoute);
     }
 }
